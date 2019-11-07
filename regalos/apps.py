783219d2c127354146616bbb33f0afcef5f73a3d
cmr451 <cmr_cristian26@hotmail.com>
2019-11-07 10:04:26 -0500

    proyecto

---
+++ regalos/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RegalosConfig(AppConfig):
+    name = 'regalos'
---
+++ regalos/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Login(models Model):
+	codigo = models.AutoField(primary_key = True)
+	Nombre = models.CharField(max_Length =30)
+	Apellido = models.CharField(max_Length =30)
+	Nombre = models.CharField(max_Length =30)
+	
+	
+		
